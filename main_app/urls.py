ef3b2e59885fdc94a7dc4d415212be3e06022a24
Diana Barney <diana.aiken@gmail.com>
2021-11-22 22:20:24 -0500

    added login and logout for authentication

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ========== GENERAL Routes ==========
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    # ========== AUTHENTICATION Routes ==========
+    path('classroom/', views.classroom_index, name='classroom_index'),
+
+
+    # ========== CLASSROOM Routes ==========
+
+
+    # ========== ITEM Routes ==========
+
+    # ========== WISHLIST Routes ==========
+    # ----- (associating Items to classroom)
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,35 @@
 from django.shortcuts import render
+# ----- Authentication -----
+from django.contrib.auth import login
+
+# todo - delete once no longer needed
+from django.http import HttpResponse
 
 
 
+# ========== GENERAL Views ==========
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+# ========== AUTHENTICATION Views ==========
+
+
+# ========== CLASSROOM Views ==========
+# todo - added here simply to verify authentication working - either:
+# 1. keep (add an actual html template with content & update the view function)
+# -- or -- 
+# 2. remove & change out the LOGIN_REDIRECT_URL on settings.py to a different url
+def classroom_index(request):
+    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+
+# ========== WISHLIST Views (i.e., - associate item with classroom) ==========
+
+
+# ========== ITEM Views ==========
+
+
+
