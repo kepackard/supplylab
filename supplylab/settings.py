1cb91edecd39f75259c0f55c9da4632beaab05be
Diana Barney <diana.aiken@gmail.com>
2021-12-02 14:20:29 -0500

    wip - config deployment

--- supplylab/settings.py
+++ supplylab/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 # from dotenv import load_dotenv
 # load_dotenv()   # loads the configs from .env
-# from decouple import config
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = str(os.getenv('SECRET_KEY'))   # if using python-dotenv
-# SECRET_KEY = config('SECRET_KEY')         # if not using dotenv
+SECRET_KEY = config('SECRET_KEY')         # if not using dotenv
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -125,13 +125,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 # STATIC_URL = '/main_app/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage',
 
