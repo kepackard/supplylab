2d2a2a34fe82237d5e1846c3a3a5bf36279bdbf4
Diana Barney <diana.aiken@gmail.com>
2021-12-02 13:15:02 -0500

    wip - config deployment

--- supplylab/settings.py
+++ supplylab/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()   # loads the configs from .env
+from decouple import config
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'supplylab.urls'
@@ -122,14 +124,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_URL = '/main_app/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+
 
 # redirect user after successful login
 LOGIN_REDIRECT_URL = '/classrooms/'
 # redirect user after successful logout
 LOGOUT_REDIRECT_URL = '/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = '/main_app/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
